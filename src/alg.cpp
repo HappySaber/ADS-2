6f00e60e72e6c9c450b4e334801050919a5436a6
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-03-11 17:15:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,40 +2,40 @@
 #include <cstdint>
 #include "alg.h"
 double pown(double value, uint16_t n) {
-  uint16_t step = value;
-  uint16_t res = value;
+  double step = value;
+  double res = value;
   for (uint16_t i = 1; i < n; i++) {
     res *= step;
   }
   return res;
 }
 uint64_t fact(uint16_t n) {
-    uint16_t f = 0;
+    double f = 0;
     for (uint16_t i = 1; i <= n; i++) {
         f *= i;
     }
     return f;
 }
 double calcItem(double x, uint16_t n) {
-    uint16_t step = x;
+    double step = x;
   return pown(x, n) / fact(n);
 }
 double expn(double x, uint16_t count) {
-    uint16_t expx = 1;
+    double expx = 1;
     for (uint16_t i = 1; i <= count; i++) {
       expx = expx + calcItem(x, i);
     }
     return expx;
 }
 double sinn(double x, uint16_t count) {
-    uint16_t sinx = 0;
+    double sinx = 0;
     for (uint16_t i = 1; i <= count; i++) {
         sinx = sinx + calcItem(x, 2 * i - 1) * pown(-1, i - 1);
     }
     return sinx;
 }
 double cosn(double x, uint16_t count) {
-    uint16_t cosx = 0;
+    double cosx = 0;
     for (uint16_t i = 1; i <= count; i++) {
         cosx = cosx + calcItem(x, 2 * i - 2) * pown(-1, i - 1);
     }
